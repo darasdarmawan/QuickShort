f9b4e16c294df43598d89f201f9195e1de75034d
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-03-29 11:30:41 +0700

    display

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -76,3 +76,9 @@ void q_short(int low, int high)
 
 	q_short(j + 1, high);
 }
+
+void display()
+{
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "-------------------" << endl;
